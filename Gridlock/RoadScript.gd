5ae7c79eaa51355961ad836b55d90cd2e49331b6
Rory <scobier@email.arizona.edu>
2021-03-13 21:28:20 -0700

    Added Road Connections

--- Gridlock/RoadScript.gd
+++ Gridlock/RoadScript.gd
@@ -1,9 +1,12 @@
 extends Area2D
 
 
+
 # Declare member variables here. Examples:
 var cars = []
 
+export(NodePath) var connection_1
+export(NodePath) var connection_2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,6 +19,7 @@ func _ready():
 
 func _on_Area2D_body_enter(body):
 	cars.append(body)
+	print(body + "entered road " + self)
 	
 func _on_Area2D_body_exit(body):
-	cars.erase(body)
+	cars.erase(body + "exited road " + self)
